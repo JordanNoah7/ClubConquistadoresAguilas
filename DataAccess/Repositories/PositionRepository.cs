9bc5dac6b296b05098c8872bb33896bac6888546
dyfmeks <j.jordan.quispe.supo@gmail.com>
2023-06-15 16:18:49 -0500

    PPA, PPU, Position, Role and Specialty Repository

---
+++ DataAccess/Repositories/PositionRepository.cs
@@ -0,0 +1,77 @@
+﻿using Infrastructure.Context;
+using Models;
+
+namespace DataAccess.Repositories;
+
+public class PositionRepository:ContextRepository, IGenericRepository<Position>
+{
+    public PositionRepository(ClubConquistadoresAguilasContext context) : base(context) { }
+
+    public async Task<bool> Insert(Position model)
+    {
+        try
+        {
+            _dbContext.Positions.Add(model);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> Update(Position model)
+    {
+        try
+        {
+            _dbContext.Positions.Update(model);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> Delete(int id)
+    {
+        try
+        {
+            Position model = _dbContext.Positions.First(p => p.Id == id);
+            _dbContext.Positions.Remove(model);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<Position> Get(int id)
+    {
+        try
+        {
+            return await _dbContext.Positions.FindAsync(id);
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+
+    public async Task<IQueryable<Position>> GetAll()
+    {
+        try
+        {
+            IQueryable<Position> queryPositionsSQL = _dbContext.Positions;
+            return queryPositionsSQL;
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+}
---
+++ DataAccess/Repositories/SpecialtyRepository.cs
@@ -0,0 +1,77 @@
+﻿using Infrastructure.Context;
+using Models;
+
+namespace DataAccess.Repositories;
+
+public class SpecialtyRepository: ContextRepository, IGenericRepository<Specialty>
+{
+    public SpecialtyRepository(ClubConquistadoresAguilasContext context) : base(context) { }
+
+    public async Task<bool> Insert(Specialty model)
+    {
+        try
+        {
+            _dbContext.Specialties.Add(model);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> Update(Specialty model)
+    {
+        try
+        {
+            _dbContext.Specialties.Update(model);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<bool> Delete(int id)
+    {
+        try
+        {
+            Specialty model = _dbContext.Specialties.First(c => c.Id == id);
+            _dbContext.Specialties.Remove(model);
+            await _dbContext.SaveChangesAsync();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public async Task<Specialty> Get(int id)
+    {
+        try
+        {
+            return await _dbContext.Specialties.FindAsync(id);
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+
+    public async Task<IQueryable<Specialty>> GetAll()
+    {
+        try
+        {
+            IQueryable<Specialty> querySpecialtiesSQL = _dbContext.Specialties;
+            return querySpecialtiesSQL;
+        }
+        catch (Exception ex)
+        {
+            return null;
+        }
+    }
+}
