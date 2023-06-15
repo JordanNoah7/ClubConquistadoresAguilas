4f01783b912752f87e72e154e891f0e512ab676a
NancyHuamanC <n.huaman.cruz@gmail.com>
2023-06-15 13:08:05 -0500

    esqueleto login

---
+++ Web/Controllers/LoginController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+    
+}
