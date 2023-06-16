8228ed9dbed76e11b1d49aa18856b5b5617f4d59
NancyHuamanC <n.huaman.cruz@gmail.com>
2023-06-16 14:45:36 -0500

    Agregue Responsividad

---
+++ Web/Controllers/DirectorController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class DirectorController : Controller
+    {
+        public IActionResult Director()
+        {
+            return View();
+        }
+
+    }
+    
+}
