7fe2ba7d742ce1b778cab9cd73698c4250036c41
NancyHuamanC <n.huaman.cruz@gmail.com>
2023-06-23 15:14:19 -0500

    listo nav- no responsive

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -5,12 +5,22 @@
 
 const allSideMenu = document.querySelectorAll('#sidebar .side-menu li a');
 
-allSideMenu.forEach(item => {
-    const li = item.parentElement;
-    item.addEventListener('click', function () {
-        allSideMenu.forEach(i => { 
-            i.parentElement.classList.remove('active');
+        allSideMenu.forEach(item => {
+            const li = item.parentElement;
+            item.addEventListener('click', function () {
+                allSideMenu.forEach(i => { 
+                    i.parentElement.classList.remove('active');
+                })
+                li.classList.add('active');
+            })
+        });
+
+        //togle sidebar
+        const menuBar = document.querySelector('#content nav .bx.bx-menu')
+        const sideBar = document.getElementById('sidebar');
+
+        menuBar.addEventListener('click', function () {
+            
+            sideBar.classList.toggle('hide');
         })
-        li.classList.add('active');
-    })
-});
+
