fd611dcb8d78ae1538556a3f03ccfe73954d14c8
NancyHuamanC <n.huaman.cruz@gmail.com>
2023-06-22 23:01:09 -0500

    Agregue todo el navbar

--- Web/wwwroot/js/site.js
+++ Web/wwwroot/js/site.js
@@ -2,3 +2,15 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+const allSideMenu = document.querySelectorAll('#sidebar .side-menu li a');
+
+allSideMenu.forEach(item => {
+    const li = item.parentElement;
+    item.addEventListener('click', function () {
+        allSideMenu.forEach(i => { 
+            i.parentElement.classList.remove('active');
+        })
+        li.classList.add('active');
+    })
+});
