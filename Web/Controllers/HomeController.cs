8faefe64aabca8194398df6860239f812c999681
NancyHuamanC <n.huaman.cruz@gmail.com>
2023-06-24 10:44:57 -0500

    navbar direccionando a paginas

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -23,6 +23,14 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult DirectorConsejero()
+    {
+        return View();
+    }
+    public IActionResult DirectorInstructor()
+    {
+        return View();
+    }
     public IActionResult Privacy()
     {
         return View();
