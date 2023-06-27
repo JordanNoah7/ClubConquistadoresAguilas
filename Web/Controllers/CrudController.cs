c4b9f0cea44d872c58e628b8bbc5dc605d3391f0
JordanNoah7 <j.jordan.quispe.supo@gmail.com>
2023-06-27 00:16:10 -0500

    Actualizacion de todo

--- Web/Controllers/CrudController.cs
+++ Web/Controllers/CrudController.cs
@@ -10,10 +10,12 @@ public class CrudController : Controller
         return View();
     }
 
+
     public ActionResult CrudConquistador()
     {
         return View();
     }
+
 
     public ActionResult CrudConsejero()
     {
@@ -25,17 +27,25 @@ public class CrudController : Controller
         return View();
     }
 
+    public ActionResult Actividades()
+    {
+        return View();
+    }
+
+
     // GET: CrudController/Details/5
     /*public ActionResult Details(int id)
     {
         return View();
-    }
+    }*/
+
 
     // GET: CrudController/Create
     public ActionResult Create()
     {
         return View();
     }
+
 
     // POST: CrudController/Create
     [HttpPost]
@@ -44,103 +54,14 @@ public class CrudController : Controller
     {
         try
         {
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Inicio));
         }
         catch
         {
             return View();
         }
-<<<<<<< HEAD
-=======
-
-        public ActionResult CrudConquistador()
-        {
-            return View();
-        }
-
-        public ActionResult CrudConsejero()
-        {
-            return View();
-        }
-        public ActionResult CrudInstructor()
-        {
-            return View();
-        }
-        public ActionResult Actividades()
-        {
-            return View();
-        }
-
-        // GET: CrudController/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        // GET: CrudController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: CrudController/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Inicio));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: CrudController/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: CrudController/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Inicio));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: CrudController/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: CrudController/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                return RedirectToAction(nameof(Inicio));
-            }
-            catch
-            {
-                return View();
-            }
-        }
->>>>>>> 5341ec88b4a9b921a93f05307ee80140179723ee
     }
+
 
     // GET: CrudController/Edit/5
     public ActionResult Edit(int id)
@@ -148,20 +69,22 @@ public class CrudController : Controller
         return View();
     }
 
+
     // POST: CrudController/Edit/5
-    [HttpPost]
+    /*[HttpPost]
     [ValidateAntiForgeryToken]
     public ActionResult Edit(int id, IFormCollection collection)
     {
         try
         {
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Inicio));
         }
         catch
         {
             return View();
         }
-    }
+    }*/
+
 
     // GET: CrudController/Delete/5
     public ActionResult Delete(int id)
@@ -169,14 +92,15 @@ public class CrudController : Controller
         return View();
     }
 
+
     // POST: CrudController/Delete/5
-    [HttpPost]
+    /*[HttpPost]
     [ValidateAntiForgeryToken]
     public ActionResult Delete(int id, IFormCollection collection)
     {
         try
         {
-            return RedirectToAction(nameof(Index));
+            return RedirectToAction(nameof(Inicio));
         }
         catch
         {
