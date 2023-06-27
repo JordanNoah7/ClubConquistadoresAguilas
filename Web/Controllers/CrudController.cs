f95dd59a744c9bdffe66d4b2c5c65776b1e13868
JordanNoah7 <j.jordan.quispe.supo@gmail.com>
2023-06-27 10:53:07 -0500

    Correccion de error de json

--- Web/Controllers/CrudController.cs
+++ Web/Controllers/CrudController.cs
@@ -1,113 +1,101 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
-namespace Web.Controllers;
-
-public class CrudController : Controller
+namespace Web.Controllers
 {
-    // GET: CrudController
-    public ActionResult Inicio()
+    public class CrudController : Controller
     {
-        return View();
-    }
-
-
-    public ActionResult CrudConquistador()
-    {
-        return View();
-    }
-
-
-    public ActionResult CrudConsejero()
-    {
-        return View();
-    }
-
-    public ActionResult CrudInstructor()
-    {
-        return View();
-    }
-
-    public ActionResult Actividades()
-    {
-        return View();
-    }
-
-
-    // GET: CrudController/Details/5
-    /*public ActionResult Details(int id)
-    {
-        return View();
-    }*/
-
-
-    // GET: CrudController/Create
-    public ActionResult Create()
-    {
-        return View();
-    }
-    
-    // GET: CrudController/Edit/5
-    public ActionResult Edit(int id)
-    {
-        return View();
-    }
-
-    // GET: CrudController/Delete/5
-    public ActionResult Delete(int id)
-    {
-        return View();
-    }
-
-    // POST: CrudController/Create
-    /*[HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Create(IFormCollection collection)
-    {
-        try
-        {
-            return RedirectToAction(nameof(Inicio));
-        }
-        catch
+        // GET: CrudController
+        public ActionResult Inicio()
         {
             return View();
         }
-    }
 
-
-    
-
-
-    // POST: CrudController/Edit/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Edit(int id, IFormCollection collection)
-    {
-        try
-        {
-            return RedirectToAction(nameof(Inicio));
-        }
-        catch
+        public ActionResult CrudConquistador()
         {
             return View();
         }
-    }
 
-
-    
-
-
-    // POST: CrudController/Delete/5
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public ActionResult Delete(int id, IFormCollection collection)
-    {
-        try
-        {
-            return RedirectToAction(nameof(Inicio));
-        }
-        catch
+        public ActionResult CrudConsejero()
         {
             return View();
         }
-    }*/
+        public ActionResult CrudInstructor()
+        {
+            return View();
+        }
+        public ActionResult Actividades()
+        {
+            return View();
+        }
+
+        // GET: CrudController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: CrudController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CrudController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Inicio));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CrudController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: CrudController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Inicio));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: CrudController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: CrudController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Inicio));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
 }
