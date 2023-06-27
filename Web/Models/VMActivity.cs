6faa02da8c7af016c5fb7525a3dd80ce9c031498
Roberto <r.apaza.cornejo@gmail.com>
2023-06-27 17:17:42 -0500

    Llenando VMActivity

--- Web/Models/VMActivity.cs
+++ Web/Models/VMActivity.cs
@@ -2,5 +2,20 @@
 {
     public class VMActivity
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+
+        public string StartDate { get; set; }
+
+        public string EndDate { get; set; }
+
+        public string Location { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public string Requirements { get; set; } = null!;
+
+        public int ClubId { get; set; }
     }
 }
