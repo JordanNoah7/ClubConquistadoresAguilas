32ca0c27d5517b3b4305d17dae10199050935588
JordanNoah7 <j.jordan.quispe.supo@gmail.com>
2023-06-30 11:45:05 -0500

    cambios para adonet

---
+++ DataAccess/UserPersonRepository.cs
@@ -0,0 +1,25 @@
+﻿using Domain;
+using Models;
+
+namespace DataAccess;
+
+public class UserPersonRepository : IUserPersonRepository
+{
+    public async Task<bool> Insert(User user, Person person)
+    {
+        try
+        {
+            
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    public Task<bool> Delete(int id1)
+    {
+        throw new NotImplementedException();
+    }
+}
