85f9a247d625953ee636ce465ec3a96bfbd4bccb
NancyHuamanC <131500629+NancyNdo@users.noreply.github.com>
2023-07-07 11:36:08 -0500

    estilos en inicio y estilos modificados en vista create

---
+++ Web/wwwroot/js/script.js
@@ -0,0 +1,58 @@
+﻿
+const searchcontainer = document.querySelector('.buscar')
+const inputsearch = searchcontainer.querySelector('input')
+const boxsugerencias = document.querySelector('.listaPadres')
+
+inputsearch.onkeyup = e => {
+    let userdata = e.target.value;
+    let emptyArray = [];
+    if (userdata) {
+        emptyArray = suggestions.filter(data => {
+            return data
+                .toLocaleLowerCase()
+                .startsWith(userdata.toLocaleLowerCase());
+
+        });
+        emptyArray = emptyArray.map( data => {
+            return (data = `<li>${data}</li>`);
+        });
+        searchcontainer.classList.add('active')
+        mostrarpadres(emptyArray);
+
+
+        let allList = boxsugerencias.querySelectorAll('li');
+
+        allList.forEach(li => {
+            li.setAttribute('onclick', 'select(this)');
+        });
+    }
+    else {
+        searchcontainer.classList.remove('active');
+    }
+};
+
+function select(element) {
+    let selectUserData = element.textContent;
+    inputsearch.value = selectUserData;
+    searchcontainer.classList.remove('active');
+}
+
+
+
+const mostrarpadres = list => {
+    let listData;
+
+    if (!list.length) {
+
+        //muestra el mismo valor que estaba buscando..
+        //userValue = inputsearch.value;
+        //listData = `<li>${userValue}</li>`;
+        listData = `<li>"coincidencia no encontrada"</li>`;
+    } else {
+        listData = list.join(' ');
+    }
+
+    boxsugerencias.innerHTML = listData;
+}
+
+
---
+++ Web/wwwroot/js/suggestions.js
@@ -0,0 +1,14 @@
+﻿let suggestions = [
+    'Pablito Suarez',
+    'Hugo Bonito',
+    'Mateo Cueva',
+    'Martín Ocaña Palacios',
+    'Lucas Chara',
+    'Leo Dan',
+    'Daniel Atayupanqui',
+    'Alejandro Hinojosa',
+    'Manuel Armando Rascacielos',
+    'Mitín Ocaña Palacios',
+    'Mthutín Ocaña Palacios',
+    'Mert Ocaña Palacios',
+];
